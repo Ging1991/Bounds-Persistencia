e5018f2b1a4277cfaac2efa284e94b00db166e12
Ging1991 <carloscaballeromorel@gmail.com>
2026-08-16 01:52:30 -0300

    Se agrega idiomas

--- Runtime/Traductores/ControlUIBounds.cs
+++ Runtime/Traductores/ControlUIBounds.cs
@@ -1,21 +1,20 @@
 using Bounds.Persistencia.proveedores;
+using Ging1991.Idiomas;
 using Ging1991.Interfaces.Personalizacion;
 using Ging1991.Persistencia.Lectores;
 using Ging1991.Persistencia.Proveedores;
 using UnityEngine;
 
-namespace Bounds.Persistencia {//
+namespace Bounds.Persistencia {
 
 	public class ControlUIBounds : MonoBehaviour {
 
 		public ControlUI controlUI;
 
 		public void Personalizar(string direccionTexto, string direccionColor) {
-			controlUI.Inicializar(
-				new ProveedorColores(direccionColor, TipoLector.RECURSOS),
-				new ProveedorTexto(direccionTexto, TipoLector.RECURSOS)
-			);
-			controlUI.ActualizarUI();
+			/*controlUI.Inicializar(new ProveedorColores(direccionColor, TipoLector.RECURSOS));
+			Traductor.Instancia.Inicializar(new ProveedorTexto(direccionTexto, TipoLector.RECURSOS));
+			controlUI.ActualizarUI();*/
 		}
 
 	}
